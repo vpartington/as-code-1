47d803d2137caa41ebd990003d4b1dfc5896f8a4
brhahlen <git@benweb.eu>
2016-10-12 15:12:21 +0200

    Modified CLI

--- xl.sh
+++ xl.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+# This is the CLI mockup
+
+case ${1} in
+"preview"
+"deploy"
+"list"
+"help")
+echo "xl is a command line interface to control deployments, preview them, or list applications installed"
+echo "Usage is:"
+echo "preview : previews the deployment and shows you which steps it will execute"
+echo "deploy  : deploys the application to the specified environment"
+echo "list    : list installed applications on the XLD server"
+exit
+;;
+esac
