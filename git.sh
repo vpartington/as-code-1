c0a517ade5d6554f4681fc66c0b37c6829012b0c
brhahlen <git@benweb.eu>
2016-10-13 09:55:47 +0200

    Added git mockup

---
+++ git.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#This is a git mockup
+case ${1} in
+"commit")
+echo "[master c20e310] Updated
+1 file changed, 1 insertion(+)"
+;;
+"push")
+echo "warning: push.default is unset; its implicit value has changed in
+Git 2.0 from 'matching' to 'simple'. To squelch this message
+and maintain the traditional behavior, use:
+
+  git config --global push.default matching
+
+To squelch this message and adopt the new behavior now, use:
+
+  git config --global push.default simple
+
+When push.default is set to 'matching', git will push local branches
+to the remote branches that already exist with the same name.
+
+Since Git 2.0, Git defaults to the more conservative 'simple'
+behavior, which only pushes the current branch to the corresponding
+remote branch that 'git pull' uses to update the current branch.
+
+See 'git help config' and search for 'push.default' for further information.
+(the 'simple' mode was introduced in Git 1.7.11. Use the similar mode
+'current' instead of 'simple' if you sometimes use older versions of Git)
+
+Counting objects: 4, done.
+Delta compression using up to 8 threads.
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (4/4), 345 bytes | 0 bytes/s, done.
+Total 4 (delta 2), reused 0 (delta 0)
+remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+To git@github.com:xebialabs/xfile.git
+   30651d6..c20e310  master -> master
+"
+;;
+esac
